2817b5293432e1e5c9e1cc791b9f9ce97710a49d
eaeidsath <evelyneidsath@gmail.com>
2024-02-11 21:06:06 -0800

    start work on homepage html, adjust foreign key relations in models

--- models/index.js
+++ models/index.js
@@ -12,20 +12,21 @@ Post.belongsTo(User, {
 });
 
 User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(User, {
-    foreignKey: 'user_id'
+  foreignKey: 'user_id'
 });
 
 Post.hasMany(Comment, {
-    foreignKey: 'user_id'
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(Post, {
-    foreignKey: 'user_id'
+  foreignKey: 'post_id'
 });
 
 module.exports = { User, Post, Comment };
