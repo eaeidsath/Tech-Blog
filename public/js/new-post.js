5c02370560cb2224812e20177ee68fcacce16c61
EE <evelyneidsath@gmail.com>
2024-03-01 16:01:28 -0800

    adjust styling, add delete post functionality. these files are part of previous commit

--- public/js/new-post.js
+++ public/js/new-post.js
@@ -18,3 +18,40 @@ const postFormHandler = async (event) => {
       }
     }
 };
+
+const deletePost = async (id) => {
+  try {
+    const response = await fetch('/api/posts/' + id,  {
+      method: 'DELETE'
+    })
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post.');
+    }
+  } catch (error) {
+    console.error('Delete error:', error);
+    alert('An error occurred while deleting post.');
+  }
+};
+
+const updatePost = async (id) => {
+  const title = document.querySelector('.post-title').value;
+  const post_body = document.querySelector('.post-body').value;
+
+  if (title && post_body) {
+    const response = await fetch('/api/posts/' + id, {
+      method: 'PUT',
+      body: JSON.stringify({ title, post_body }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      console.log(response);
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to update post.');
+    }
+  }
+}
